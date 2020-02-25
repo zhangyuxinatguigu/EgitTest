5bd48598efe8d5921b63b4b794bc5937a7d63d61
zhangxinatguigu <879808653@qq.com>
2020-02-25 20:28:52 +0800

    在学习，你呢？

--- src/com/atguigu/gittest/sole.java
+++ src/com/atguigu/gittest/sole.java
@@ -3,5 +3,6 @@ package com.atguigu.gittest;
 public class sole {
 	public static void main(String[] args) {
 		System.out.println("在干嘛呢--zhangyuxinatguigu");
+		System.out.println("在学习，你呢？");
 	}
 }
